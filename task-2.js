1215beb13a643040d5389bce19ace6841943c933
Marina <kulinicmarina91@gmail.com>
2022-12-07 00:41:43 +0300

    this is the second task i am practicing with the function

---
+++ task-2.js
@@ -0,0 +1,11 @@
+function getNextNumber(num) {
+    if(num == 0) {
+        return 0;
+    } 
+    else if(num > 0) {
+        return num + 1;
+    }
+    else if(num < 0) {
+        return num - 1;
+    }
+}
